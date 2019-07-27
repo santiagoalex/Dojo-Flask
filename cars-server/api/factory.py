46e04e21c46b7ff92370a543c769316c69b5ba2e
Giovanny <gio96gomez@gmail.com>
2019-07-27 09:47:08 -0500

    first commit

---
+++ cars-server/api/factory.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+
+from flask_pymongo import PyMongo
+from flask_restful import Api
+
+app = Flask(__name__)
+
+# Configure CORS
+cors = CORS(app,  origins=['http://localhost:8080'])
+
+# Configure database
+app.config["MONGO_URI"] = "mongodb://3.14.7.212:27017/Cars"
+app.database = PyMongo(app)
+app.cars_collection = app.database.db.cars
+
+# Create api
+api = Api(app)
+
+from api.controller.car import Car
+from api.controller.car import CarList
+
+#...
+#...
+#...
+
+api.add_resource(Car, '/car', '/car/<car_id>',
+                 methods=['GET', 'POST', 'PUT', 'DELETE'])
+
+api.add_resource(CarList, '/cars',
+                 methods=['GET'])
